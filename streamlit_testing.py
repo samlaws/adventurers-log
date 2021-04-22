be59e4d4eeec7876d794a5c4682c4361d56855e7
Samuel <samuel.laws@ust-global.com>
2021-04-22 14:21:24 +0100

    interactive graphing

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -6,10 +6,11 @@ from vega_datasets import data
 
 from api_testing import ApiMethods
 from snapshots_to_pandas import snapshot_to_skills
-from utils import load_skill_icons
+from utils import skill_layout
+
 
 username = st.sidebar.text_input("Enter a username", value='', max_chars=12)
-# username = "pompelmo"
+#username = "pompelmo"
 if username:
     api = ApiMethods(username=username)
 
@@ -20,47 +21,76 @@ if username:
         period = st.selectbox('Tracking period:',
                               ('6h', 'day', 'week', 'month', 'year'), index=2)
 
-        skill_layout = load_skill_icons()
-
         player_data = api.get_player_snapshots(id=msg, period=period)
 
-        try:
-            skill_df = snapshot_to_skills(player_data)
+        skill_df = snapshot_to_skills(player_data)
 
-            melt = skill_df.reset_index().melt(id_vars=['date'])
-            melt["date"] = pd.to_datetime(melt["date"])
-            overall = melt[melt["variable"] == "overall"]
-            melt = melt[melt["variable"] != "overall"]
+        melt = skill_df.reset_index().melt(id_vars=['date'])
+        melt["date"] = pd.to_datetime(melt["date"])
 
-            cols = st.beta_columns((1, 1, 1, 7))
+        melt.drop_duplicates(subset=["variable", "value"])
 
-            # create tilegrid with select boxes for each skill
-            # can then filter 'melt' by those skills in the chart
-            filter_skills = []
-            for row in skill_layout:
-                for i in range(len(row)):
-                    # st.image(row[i][1]))
-                    check = cols[i].checkbox(row[i][0])
-                    if check:
-                        filter_skills.append(row[i][0])
+        overall = melt[melt["variable"] == "overall"]
+        melt = melt[melt["variable"] != "overall"]
 
+        filter_skills = []
+
+        skill_array = np.array(skill_layout, dtype="object")
+        n_rows, n_cols = len(skill_array[0]), len(skill_array)
+        cols = st.beta_columns(n_cols-1)
+        for c in range(n_cols-1):
+            for r in range(n_rows):
+                check = cols[c].checkbox(skill_array[c][r][1].lower())
+                if check:
+                    filter_skills.append(skill_array[c][r][0])
+
+        if filter_skills:
             print(filter_skills)
+            chart_data = melt[melt["variable"].isin(filter_skills)]
+            print(chart_data)
+        else:
+            chart_data = overall
+            print(chart_data)
 
-            if filter_skills:
-                chart_data = melt[melt["variable"].isin(filter_skills)]
-            else:
-                chart_data = overall
+        # Create a selection that chooses the nearest point & selects based on x-value
+        nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                                fields=['date'], empty='none')
 
-            c = alt.Chart(chart_data).mark_line(point=True).encode(
-                x='date',
-                y='value',
-                color='variable'
-            )
-
-            cols[3].altair_chart(c, use_container_width=True)
-
-        except IndexError:
-            st.write("No data available from that period")
+        # The basic line
+        line = alt.Chart(chart_data).mark_line(interpolate="linear").encode(
+            x='date:T',
+            y='value:Q',
+            color='variable:N'
+        )
+        # Transparent selectors across the chart. This is what tells us
+        # the x-value of the cursor
+        selectors = alt.Chart(chart_data).mark_point().encode(
+            x='date:T',
+            opacity=alt.value(0),
+        ).add_selection(
+            nearest
+        )
+        # Draw points on the line, and highlight based on selection
+        points = line.mark_point().encode(
+            opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+        )
+        # Draw text labels near the points, and highlight based on selection
+        text = line.mark_text(align='left', dx=5, dy=-5).encode(
+            text=alt.condition(nearest, 'value:Q', alt.value(' '))
+        )
+        # Draw a rule at the location of the selection
+        rules = alt.Chart(chart_data).mark_rule(color='gray').encode(
+            x='date:T',
+        ).transform_filter(
+            nearest
+        )
+        # Put the five layers into a chart and bind the data
+        st.altair_chart(alt.layer(
+            line, selectors, points, rules, text
+        ), use_container_width=True)
 
     else:
         st.sidebar.write("Player not found")
+
+else:
+    st.write("Enter a username in the side bar to get started...")
--- utils.py
+++ utils.py
@@ -1,38 +1,9 @@
-import requests
-from PIL import Image
-from io import BytesIO
-import streamlit as st
-
-
-@st.cache()
-def load_skill_icons():
-
-    skill_layout = [[["attack", "https://oldschool.runescape.wiki/images/f/fe/Attack_icon.png?b4bce"],
-                     ["hitpoints", "https://oldschool.runescape.wiki/images/9/96/Hitpoints_icon.png?a4819"],
-                     ["mining", "https://oldschool.runescape.wiki/images/4/4a/Mining_icon.png?00870"]],
-                    [["strength", "https://oldschool.runescape.wiki/images/1/1b/Strength_icon.png?e6e0c"], ["agility",
-                                                                                                            "https://oldschool.runescape.wiki/images/8/86/Agility_icon.png?389e0"], ["smithing", "https://oldschool.runescape.wiki/images/d/dd/Smithing_icon.png?d26c5"]],
-                    [["defence", "https://oldschool.runescape.wiki/images/b/b7/Defence_icon.png?ca0cd"], ["herblore",
-                                                                                                          "https://oldschool.runescape.wiki/images/0/03/Herblore_icon.png?ffa9e"], ["fishing", "https://oldschool.runescape.wiki/images/3/3b/Fishing_icon.png?15a98"]],
-                    [["ranged", "https://oldschool.runescape.wiki/images/1/19/Ranged_icon.png?01b0e"], ["thieving",
-                                                                                                        "https://oldschool.runescape.wiki/images/4/4a/Thieving_icon.png?973fe"], ["cooking", "https://oldschool.runescape.wiki/images/d/dc/Cooking_icon.png?a0156"]],
-                    [["prayer", "https://oldschool.runescape.wiki/images/f/f2/Prayer_icon.png?ca0dc"], ["crafting",
-                                                                                                        "https://oldschool.runescape.wiki/images/c/cf/Crafting_icon.png?a1f71"], ["firemaking", "https://oldschool.runescape.wiki/images/9/9b/Firemaking_icon.png?45ea0"]],
-                    [["magic", "https://oldschool.runescape.wiki/images/5/5c/Magic_icon.png?334cf"], ["fletching", "https://oldschool.runescape.wiki/images/9/93/Fletching_icon.png?15cda"],
-                        ["woodcutting", "https://oldschool.runescape.wiki/images/f/f4/Woodcutting_icon.png?6ead4"]],
-                    [["runecrafting", "https://oldschool.runescape.wiki/images/6/63/Runecraft_icon.png?c278c"], ["slayer",
-                                                                                                                 "https://oldschool.runescape.wiki/images/archive/2/28/20141020205814%21Slayer_icon.png?0741e"], ["farming", "https://oldschool.runescape.wiki/images/f/fc/Farming_icon.png?558fa"]],
-                    [["construction", "https://oldschool.runescape.wiki/images/f/f6/Construction_icon.png?f9bf7"],
-                        ["hunter", "https://oldschool.runescape.wiki/images/d/dd/Hunter_icon.png?8762f"]]
-                    ]
-
-    for row in skill_layout:
-        for skill in row:
-            response = requests.get(skill[1])
-            skill[1] = Image.open(BytesIO(response.content))
-
-    print(skill_layout)
-    return skill_layout
-
-
-load_skill_icons()
+skill_layout = [[["attack", "ATT"], ["hitpoints", "HP"], ["mining", "MIN"]],
+                [["strength", "STR"], ["agility", "AGI"], ["smithing", "SMI"]],
+                [["defence", "DEF"], ["herblore", "HER"], ["fishing", "FSH"]],
+                [["ranged", "RNG"], ["thieving", "THV"], ["cooking", "COOK"]],
+                [["prayer", "PRY"], ["crafting", "CRA"], ["firemaking", "FM"]],
+                [["magic", "MAG"], ["fletching", "FLT"], ["woodcutting", "WC"]],
+                [["runecrafting", "RC"], ["slayer", "SLY"], ["farming", "FAR"]],
+                [["construction", "CON"], ["hunter", "HUN"]]
+                ]
