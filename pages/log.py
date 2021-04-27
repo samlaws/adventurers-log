7565379b38c2b957a3b9dbfff7229940f45e11da
Samuel <samuel.laws@ust-global.com>
2021-04-27 16:56:06 +0100

    fixed dating issue

--- pages/log.py
+++ pages/log.py
@@ -18,7 +18,7 @@ def log(username):
             if status:
                 st.sidebar.write("Player found")
 
-                player_data = api.get_player_snapshots(id=msg, period="year")
+                player_data = api.get_player_snapshots(id=msg, period="week")
                 boss_df = snapshot_to_df(player_data, type="boss").drop_duplicates(
                     subset=["variable", "value"]).replace(-1, 0)
                 skill_df = snapshot_to_df(player_data, type="skills").drop_duplicates(
@@ -26,19 +26,23 @@ def log(username):
 
                 skill_df = skill_df[skill_df["variable"] != "overall"]
 
-                boss_df.sort_values(by=['variable', 'date'], inplace=True)
+                boss_df.sort_values(
+                    by=["variable", "date"], ascending=False, inplace=True)
                 boss_df['diffs'] = boss_df['value'].diff()
                 mask = boss_df.variable != boss_df.variable.shift(1)
                 boss_df['diffs'][mask] = np.nan
                 boss_df.dropna(inplace=True)
+                boss_df["diffs"] = boss_df["diffs"].abs()
 
                 st.dataframe(boss_df)
 
-                skill_df.sort_values(by=['variable', 'date'], inplace=True)
+                skill_df.sort_values(
+                    by=["variable", "date"], ascending=False, inplace=True)
                 skill_df['diffs'] = skill_df['value'].diff()
                 mask = skill_df.variable != skill_df.variable.shift(1)
                 skill_df['diffs'][mask] = np.nan
                 skill_df.dropna(inplace=True)
+                skill_df["diffs"] = skill_df["diffs"].abs()
 
                 st.dataframe(skill_df)
                 # xp gaining sessions over period
